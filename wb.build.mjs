5f691467de9cc5a9c2e839c3e82f1e9804f89f0b
Patrick O'Brien <patrick.o'brien@zywave.com>
2020-03-04 15:10:09 -0700

    feat: refresh to update pwa

--- wb.build.mjs
+++ wb.build.mjs
@@ -3,13 +3,40 @@ import workboxBuild from "workbox-build";
 // NOTE: This should be run *AFTER* all your assets are built
 const buildSW = () => {
   // This will return a Promise
-  return workboxBuild.generateSW({
-    globDirectory: '_site',
-    globPatterns: [
-      '**/*.{html,json,js,css}',
-    ],
-    swDest: '_site/service-worker.js',
-  });
+  return workboxBuild
+    .generateSW({
+      globDirectory: "_site",
+      globPatterns: ["**/*.{html,json,js,css}"],
+      swDest: "_site/service-worker.js",
+      clientsClaim: true,
+      skipWaiting: true,
+      runtimeCaching: [
+        {
+          // Match any request that ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+          // Apply a cache-first strategy.
+          handler: "CacheFirst",
+
+          options: {
+            // Use a custom cache name.
+            cacheName: "images",
+
+            // Only cache 10 images.
+            expiration: {
+              maxEntries: 10
+            }
+          }
+        }
+      ]
+    })
+    .then(({ count, size, warnings }) => {
+      for (const warning of warnings) {
+        console.warn(warning);
+      }
+      console.log(`Precached ${count} files, totalling ${size} bytes.`);
+    })
+    .catch(error => console.error(`Something went wrong: ${error}`));
 };
 
 buildSW();
